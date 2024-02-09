328cc6f0ecf6d3ed9b9bec762718d584ded1b988
Spencer Fletcher <44955744+cidarid@users.noreply.github.com>
2024-02-09 15:36:35 -0800

    Some initial modification

--- arch/src/routes/canvas/+page.svelte
+++ arch/src/routes/canvas/+page.svelte
@@ -3,45 +3,49 @@
 	let width = 1000;
 	let height = 1000;
 	let canvas: HTMLCanvasElement;
+	let roomCanvas: HTMLCanvasElement;
 
-	function parseJson() {}
-
-	onMount(async () => {
-		let json = await fetch("src/lib/test.json").then((r) => r.json());
-		console.log(json);
-		let img = new Image();
-		img.src =
-			"https://www.itftennis.com/remote.axd/media.itftennis.com/assetbank-itf/servlet/display?file=22137ab7ed210958b636fd08.jpg?crop=0.18233117483811284,0,0.27067530064754858,0&cropmode=percentage&width=219&height=282&rnd=133353579870000000";
-		let img2 = new Image();
-		img2.src = "src/lib/images/svelte-welcome.png";
+	function initCanvas(canvas: HTMLCanvasElement) {
 		let ctx = canvas.getContext("2d")!;
+		ctx.lineWidth = 3;
 		if (!ctx) {
 			throw new Error("Canvas not supported");
 		}
-		img.onload = function () {
-			ctx.drawImage(img, 0, 0); // Or at whatever offset you like
-		};
-		img2.onload = function () {
-			ctx.drawImage(img2, -500, 200); // Or at whatever offset you like
-		};
-		// Set line width
-		ctx.lineWidth = 3;
+		return ctx;
+	}
 
-		// Wall
-		ctx.strokeRect(75, 140, 150, 110);
+	onMount(async () => {
+		let ctx = initCanvas(canvas);
+		let roomCtx = initCanvas(roomCanvas);
+		if (!roomCtx) {
+			throw new Error("Canvas not supported");
+		}
+		roomCtx.lineWidth = 3;
 
-		// Door
-		ctx.fillRect(130, 190, 40, 60);
-
-		// Roof
-		ctx.beginPath();
-		ctx.moveTo(50, 140);
-		ctx.lineTo(150, 60);
-		ctx.lineTo(250, 140);
-		ctx.closePath();
-		ctx.stroke();
+		let json = await fetch("src/lib/test.json").then((r) => r.json());
+		console.log(json[0]);
+		json.forEach((room: any) => {
+			room.furniture.forEach((door: any) => {
+				let img = new Image();
+				img.src = "src/lib/images/" + door.type + ".png";
+				img.onload = function () {
+					ctx.drawImage(img, door.x, door.y, door.width, door.height);
+				};
+			});
+			roomCtx.strokeRect(room.x, room.y, room.width, room.height);
+		});
 	});
 </script>
 
-<canvas id="my-house" {width} {height} bind:this={canvas} />
+<canvas id="base" {width} {height} bind:this={canvas} />
+<canvas id="room" {width} {height} bind:this={roomCanvas} />
 <img src="src/lib/images/svelte-welcome.png" alt="Welcome" />
+
+<style>
+	canvas {
+		position: absolute;
+		left: 0;
+		top: 0;
+		z-index: 0;
+	}
+</style>
