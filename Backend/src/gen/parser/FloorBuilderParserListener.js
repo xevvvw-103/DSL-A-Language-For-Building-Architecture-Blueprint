37cd1a6eb60be53671e004b7a18ea54b0be048e7
Spike <xevvvw@gmail.com>
2024-02-25 15:47:13 -0800

    add gen flder and change some scripts

---
+++ Backend/src/gen/parser/FloorBuilderParserListener.js
@@ -0,0 +1,180 @@
+// Generated from ./parser/FloorBuilderParser.g4 by ANTLR 4.13.1
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete listener for a parse tree produced by FloorBuilderParser.
+export default class FloorBuilderParserListener extends antlr4.tree.ParseTreeListener {
+
+	// Enter a parse tree produced by FloorBuilderParser#program.
+	enterProgram(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#program.
+	exitProgram(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#statement.
+	enterStatement(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#statement.
+	exitStatement(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#make_statement.
+	enterMake_statement(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#make_statement.
+	exitMake_statement(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#object_type.
+	enterObject_type(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#object_type.
+	exitObject_type(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#width.
+	enterWidth(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#width.
+	exitWidth(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#height.
+	enterHeight(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#height.
+	exitHeight(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#name.
+	enterName(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#name.
+	exitName(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#add_statement.
+	enterAdd_statement(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#add_statement.
+	exitAdd_statement(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#direction.
+	enterDirection(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#direction.
+	exitDirection(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#target.
+	enterTarget(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#target.
+	exitTarget(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#x.
+	enterX(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#x.
+	exitX(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#y.
+	enterY(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#y.
+	exitY(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#resize_statement.
+	enterResize_statement(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#resize_statement.
+	exitResize_statement(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#repeat_statement.
+	enterRepeat_statement(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#repeat_statement.
+	exitRepeat_statement(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#repeatable_statement.
+	enterRepeatable_statement(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#repeatable_statement.
+	exitRepeatable_statement(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#times.
+	enterTimes(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#times.
+	exitTimes(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#remove_statement.
+	enterRemove_statement(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#remove_statement.
+	exitRemove_statement(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#target_object.
+	enterTarget_object(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#target_object.
+	exitTarget_object(ctx) {
+	}
+
+
+	// Enter a parse tree produced by FloorBuilderParser#base_object.
+	enterBase_object(ctx) {
+	}
+
+	// Exit a parse tree produced by FloorBuilderParser#base_object.
+	exitBase_object(ctx) {
+	}
+
+
+
+}
---
+++ Backend/src/gen/parser/FloorBuilderParserVisitor.js
@@ -0,0 +1,124 @@
+// Generated from ./parser/FloorBuilderParser.g4 by ANTLR 4.13.1
+// jshint ignore: start
+import antlr4 from 'antlr4';
+
+// This class defines a complete generic visitor for a parse tree produced by FloorBuilderParser.
+
+export default class FloorBuilderParserVisitor extends antlr4.tree.ParseTreeVisitor {
+
+	// Visit a parse tree produced by FloorBuilderParser#program.
+	visitProgram(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#statement.
+	visitStatement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#make_statement.
+	visitMake_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#object_type.
+	visitObject_type(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#width.
+	visitWidth(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#height.
+	visitHeight(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#name.
+	visitName(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#add_statement.
+	visitAdd_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#direction.
+	visitDirection(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#target.
+	visitTarget(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#x.
+	visitX(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#y.
+	visitY(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#resize_statement.
+	visitResize_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#repeat_statement.
+	visitRepeat_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#repeatable_statement.
+	visitRepeatable_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#times.
+	visitTimes(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#remove_statement.
+	visitRemove_statement(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#target_object.
+	visitTarget_object(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+	// Visit a parse tree produced by FloorBuilderParser#base_object.
+	visitBase_object(ctx) {
+	  return this.visitChildren(ctx);
+	}
+
+
+
+}
